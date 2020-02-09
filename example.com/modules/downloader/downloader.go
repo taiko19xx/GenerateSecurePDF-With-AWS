49fa194d1040475d3a1a7b9f5dca7c7641545ee5
taiko19xx <hello@taiko19xx.net>
2020-02-09 11:32:07 +0900

    uploaderとdownloaderを修正

--- example.com/modules/downloader/downloader.go
+++ example.com/modules/downloader/downloader.go
@@ -1,40 +1,42 @@
 package downloader
 
 import (
-    "github.com/aws/aws-sdk-go/aws"
-    "github.com/aws/aws-sdk-go/aws/session"
-    "github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 
 	"example.com/modules"
 
+	"fmt"
 	"os"
-    "fmt"
 )
 
 func S3Download(bucket, key string) (string, error) {
 	localPath := "/tmp/" + modules.GenRndName()
 
-	file, err := os.Create(localPath);
-	if err != nil {
-		return "", err
-    }
-    defer file.Close()
-
-	sess, _ := session.NewSession(&aws.Config{
-		Region: aws.String("ap-northeast-1")},
-	)
-	
-    downloader := s3manager.NewDownloader(sess)
-
-	_, err := downloader.Download(file,
-        &s3.GetObjectInput{
-            Bucket: aws.String(bucket),
-            Key:    aws.String(key),
-        })
+	file, err := os.Create(localPath)
 	if err != nil {
 		return "", err
 	}
-	
+	defer file.Close()
+
+	sess, _ := session.NewSession(&aws.Config{
+		Region: aws.String("ap-northeast-1"),
+	})
+
+	downloader := s3manager.NewDownloader(sess)
+
+	numBytes, err := downloader.Download(file,
+		&s3.GetObjectInput{
+			Bucket: aws.String(bucket),
+			Key:    aws.String(key),
+		})
+	if err != nil {
+		return "", err
+	}
+
+	fmt.Println("Downloaded", file.Name(), numBytes, "bytes")
+
 	return localPath, nil
 }
--- example.com/modules/uploader/uploader.go
+++ example.com/modules/uploader/uploader.go
@@ -1,35 +1,37 @@
 package uploader
 
 import (
-    "github.com/aws/aws-sdk-go/aws"
-    "github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 
+	"fmt"
 	"os"
-    "fmt"
 )
 
-func S3Upload(localPath, bucket, key string) (error) {
+func S3Upload(localPath, bucket, key string) error {
 	file, err := os.Open(localPath)
-    if err != nil {
+	if err != nil {
 		return err
 	}
 	defer file.Close()
-	
+
 	sess, _ := session.NewSession(&aws.Config{
-		Region: aws.String("ap-northeast-1")},
-	)
-	
+		Region: aws.String("ap-northeast-1"),
+	})
+
 	uploader := s3manager.NewUploader(sess)
-	
+
 	_, err = uploader.Upload(&s3manager.UploadInput{
-        Bucket: aws.String(bucket),
-        Key: aws.String(key),
-        Body: file,
-    })
-    if err != nil {
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+		Body:   file,
+	})
+	if err != nil {
 		return err
-    }
+	}
+
+	fmt.Println("Uploaded", key, bucket)
 
 	return nil
 }
